24080d4f636a96b41f4c262b5d54c6df39e57fad
Rimabo <a.amirscholar@gmail.com>
2019-09-10 19:50:14 +0200

    make search string lowercase for simplicity

--- models/Word.js
+++ models/Word.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const WordSchema = mongoose.Schema({
     word: {
         type: String,
+        lowercase: true,
         required: true
     },
     bounding_box: {
